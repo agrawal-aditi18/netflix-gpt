7ff25b93e2e993368f322ce79fd1c8340963a735
agrawal-aditi18 <aditiagrawalsofficial@gmail.com>
2025-12-30 13:55:14 +0530

    sign/signup form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './Header'
 
 const Login = () => {
+  const [isSignInForm , setIsSignInForm] = useState(true);
+  const toggleSignInForm = () => {
+    setIsSignInForm(!isSignInForm)
+  }
   return (
     <div>
       <Header />
@@ -11,11 +15,16 @@ const Login = () => {
         alt="bg-img"/>
       </div>
       <form className='absolute w-[29%] p-12 bg-black my-36 mx-auto right-0 left-0 text-white bg-opacity-80 rounded-md'>
-        <h1 className='text-3xl font-bold pb-6'>Sign In</h1>
-        <input className='rounded-sm p-4 my-2 w-full bg-transparent border-2 ' type ="text" placeholder='Email or mobile number'/>
+        <h1 className='text-3xl font-bold pb-6'>{isSignInForm ? "Sign In" : "Sign Up"}</h1>
+        {!isSignInForm && (<input className='rounded-sm p-4 my-2 w-full bg-transparent border-2 ' type ="text" placeholder='Full Name'/>)}
+        {!isSignInForm && (<input className='rounded-sm p-4 my-2 w-full bg-transparent border-2 ' type ="text" placeholder='Email'/>)}
+        {!isSignInForm && (<input className='rounded-sm p-4 my-2 w-full bg-transparent border-2 ' type ="text" placeholder='Mobile Number'/>)}
+        {isSignInForm && (<input className='rounded-sm p-4 my-2 w-full bg-transparent border-2 ' type ="text" placeholder='Email or mobile number'/>)}
         <input className='rounded-sm p-4 my-2 w-full bg-transparent border-2' type ="password" placeholder='Password' />
-        <button className='px-4 py-2 rounded-md my-2 w-full bg-red-600 font-bold'>Sign In</button>
-        <p className='py-4'>New to Netflix-gpt? <a className='font-bold'>Sign up Now.</a></p>
+        <button className='px-4 py-2 rounded-md my-2 w-full bg-red-600 font-bold'>{isSignInForm ? "Sign In" : "Sign Up"}</button>
+        <p className='py-4'><span onClick={toggleSignInForm} className='cursor-pointer font-bold'>
+        {isSignInForm? "New to Netflix-gpt? SignUp Now." : "Already a User? SignIn Now."}</span>
+        </p>
       </form>
     </div>
   )
