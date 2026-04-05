651f9fa56703ceced43ea51bb2dd2803a899df63
agrawal-aditi18 <aditiagrawalsofficial@gmail.com>
2026-04-05 17:13:22 +0530

    getData from TMDB now playing list

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,9 @@
 export const LOGO="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production_2025-12-03/consent/87b6a5c0-0104-4e96-a291-092c11350111/019ae4b5-d8fb-7693-90ba-7a61d24a8837/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
 export const USER_AVATAR="https://wallpapers.com/images/hd/netflix-profile-pictures-1000-x-1000-qo9h82134t9nv0j0.jpg";
+export const API_OPTIONS= {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMjg3NTgwOTEzN2QxNThjMmZlMTI2NjI4MDJhZDUyZCIsIm5iZiI6MTc3NTM4Nzc3OS45NjIsInN1YiI6IjY5ZDI0NDgzMzFiZWQxMzZkM2FlZjgyMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.CuIBgr7r7OK9gcdlcwGP463jEtrT8aTIScQIg7pSfxw'
+  }
+};
